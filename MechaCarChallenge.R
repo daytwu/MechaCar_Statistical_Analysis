ff9431f91aa4dcff7e44620f828bf33b348fa4c0
daytwu <daytwu@gmail.com>
2021-01-22 16:51:22 -0500

    updated

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -10,3 +10,8 @@ suscoil <- read.csv('suspension_coil.csv')
 
 total_summary <- suscoil  %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance_PSI=var(PSI), SD_PSI=sd(PSI), .groups='drop')
 lot_summary <- suscoil  %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance_PSI=var(PSI), SD_PSI=sd(PSI), .groups='drop')
+
+t_test_total_lots <- t.test(suscoil$PSI, mu=1500)
+t_test_lot1 <- t.test(subset(suscoil,Manufacturing_Lot=='Lot1')$PSI,mu=1500)
+t_test_lot2 <- t.test(subset(suscoil,Manufacturing_Lot=='Lot2')$PSI,mu=1500)
+t_test_lot3 <- t.test(subset(suscoil,Manufacturing_Lot=='Lot3')$PSI,mu=1500)
